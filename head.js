f46a9e41aa52d3dbeca1a30e0326f25709b59a98
Devon <devon.blake@live.com>
2019-07-24 04:42:46 +0000

    updated head.js

--- head.js
+++ head.js
@@ -1,19 +1,20 @@
+// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-const head = function(arr) {
-  if (arr.length <= 0) {
-    return;
-  } else {
-    return arr[0];
-  }
-};
+const head = function(array) {
+  return array[0];
+}
+
+
+
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 3);
+assertEqual(head([5]), 3);
+assertEqual(head([]), 5);
