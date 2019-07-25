bcd165e1936a528c99459facb6aac1a194b32698
Devon <devon.blake@live.com>
2019-07-25 15:45:21 +0000

    modified middle, added without

--- middle.js
+++ middle.js
@@ -42,11 +42,12 @@ const middle = function(input) {
 };
 
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
 
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
 
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
