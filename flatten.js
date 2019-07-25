d57a099e00ea99b14bd84c180563e94ca1bcdf1e
Devon <devon.blake@live.com>
2019-07-25 20:24:28 +0000

    added flatten, and findByValue

---
+++ flatten.js
@@ -0,0 +1,43 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+let assertArraysEqual = function(actual, expected) {
+  const result = eqArrays(actual, expected);
+  if (result === true) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Create a function flatten which will take in an array of arrays and return a "flattened" version of the array.
+const flatten = function(arr1) {
+  let result = []
+  for (let i = 0; i < arr1.length ;i++) {
+    const currentItem = arr1[i]
+    if (Array.isArray(currentItem)) {
+      result = result.concat(currentItem) //this is another solution array.concat
+      /*
+      for (let x = 0; x < currentItem.length; x++) {
+        result.push(currentItem[x])
+      }*/
+    } else {
+      result.push(currentItem)
+    };
+  }
+  return result
+};
+
+
+//return an array
+console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
