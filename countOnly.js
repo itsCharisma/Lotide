b1816aa5032785977d56887e13eef9ab825fac45
Devon <devon.blake@live.com>
2019-07-25 05:14:35 +0000

    added and modified some files

---
+++ countOnly.js
@@ -0,0 +1,40 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  const result = {};
+
+  for (let item of allItems) {
+    if (itemsToCount[item] === true) {
+      if (result[item] !== undefined) result[item]++;
+      else result[item] = 1;
+    }
+  }
+  return result;
+};
+
+
+
+//Test code
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
---
+++ letterPositions.js
@@ -0,0 +1,47 @@
+/*This challenge is similar to countLetters and allows us to spend some more time solving problems with objects.
+
+We'll implement a new function letterPositions which will return all the indices (zero-based positions) in the string where each character is found.
+
+For each letter, instead of returning just one number to represent its number of occurrences, multiple numbers may be needed to represent all the places in the string that it shows up.
+*/
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+let assertArraysEqual = function(actual, expected) {
+  const result = eqArrays(actual, expected);
+  if (result === true) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const letterPositions = function(sentence) {
+  let results = {};
+  sentence = sentence.toLowerCase();
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (!results[sentence[i]]) {
+        results[sentence[i]] = [i];
+      } else {
+        results[sentence[i]].push(i);
+      }
+    }
+  }
+  return results;
+};
+
+
+console.log(letterPositions("lighthouse in the house"));
+assertArraysEqual(letterPositions('hello'), {h:[0], e:[1], l:[2,3], o:[4]});
