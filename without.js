b670d12a93fdd61c1a26e26c1ab91ff33bed74a5
Devon <devon.blake@live.com>
2019-07-25 15:47:29 +0000

    modified without.js

--- without.js
+++ without.js
@@ -48,3 +48,8 @@ const isValueInArray = function(value, arr) {
 
 assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
 assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+
+const words = ["hello", "world", "lighthouse"];
+without(["hello", "world", "lighthouse"], ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
